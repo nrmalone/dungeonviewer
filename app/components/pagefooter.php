a7c0a76af0806ab1c036f681a6868ba03e276334
Nick <nicholasrmalone99@gmail.com>
2024-03-13 23:41:43 -0700

    changed footer and added user-friendly error output to character modeler view

--- app/components/pagefooter.php
+++ app/components/pagefooter.php
@@ -1,8 +1,8 @@
             <footer>
                 <a class="footerElement" href="https://www.instagram.com/" target="_blank"><img class="footerElement" alt="link to DND Viewer on Instagram" src="<?=ROOT?>img/footer/ig.png"></a>
-                <a class="footerElement" href="https://www.tiktok.com/" target="_blank"><img class="footerElement" alt="link to DND Viewer on TikTok" src="<?=ROOT?>img/footer/tt.png"></a>
-                <a style="color: white; text-decoration: none; text-decoration: underline;" class="footerElement" href="<?=ROOT?>home/about">About</a>
-                <a style="color: white; text-decoration: none; text-decoration: underline;" class="footerElement" href="<?=ROOT?>home/sitepolicies">Privacy Policy & Community Guidelines</a>
+                <a class="footerElement" href="https://www.youtube.com/" target="_blank"><img class="footerElement" alt="link to DND Viewer on YouTube" src="<?=ROOT?>img/footer/yt.png"></a>
+                <a style="color: black; text-decoration: none; text-decoration: underline;" class="footerElement" href="<?=ROOT?>home/about">About</a>
+                <a style="color: black; text-decoration: none; text-decoration: underline;" class="footerElement" href="<?=ROOT?>home/sitepolicies">Privacy Policy & Community Guidelines</a>
             </footer>
         </main>
     </body>
--- app/views/modeler.php
+++ app/views/modeler.php
@@ -1,7 +1,11 @@
 <?php require_once '../app/components/pageheader.php'; ?>
-<?php if ($data['pc']) : ?>
+<?php if ($data['pc'] !== false) : ?>
 <!-- threejs -->
 <?php else: ?>
 <!-- display error about unable to load character -->
+<div align="center" style="justify-content: center; max-width: max-content; margin: auto;">
+    <h2>Whoops! Looks like that character doesn't exist yet.</h2>
+    <h3>Want to <a href=<?=ROOT?><?php if (isset($_SESSION['userID'])) { echo "character"; } else { echo "account/signin"; } ?> style="color: white; text-decoration: none; text-decoration: underline;">make your own</a>?</h3>
+</div>
 <?php endif; ?>
 <?php require_once '../app/components/pagefooter.php'; ?>
