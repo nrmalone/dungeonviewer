9b6fffeb0a8d3142ede1daaf796db55791e8f264
nrmalone <nicholasrmalone99@gmail.com>
2024-03-16 21:27:19 -0700

    created scene with basic character model and camera controls

--- app/components/threejs.php
+++ app/components/threejs.php
@@ -12,7 +12,7 @@
             {
                 "imports": {
                     "three": "https://unpkg.com/three@0.162.0/build/three.module.js",
-                    "three/addons/": "https://unpkg.com/three@0.162.0/examples/jsm/",
+                    "GLTFLoader": "https://unpkg.com/three@0.162.0/examples/jsm/loaders/GLTFLoader.js",
                     "OrbitControls": "https://unpkg.com/three@0.162.0/examples/jsm/controls/OrbitControls.js"
                 }
             }
@@ -58,26 +58,60 @@
             <script type="module">
                 import * as THREE from 'three';
                 import {OrbitControls} from 'OrbitControls';
+                import {GLTFLoader} from 'GLTFLoader';
 
                 const renderer = new THREE.WebGLRenderer();
 
+                renderer.shadowMap.enabled = true;
                 renderer.setSize((0.85*window.innerWidth), (0.63*window.innerHeight));
                 //renderer.domElement.style.marginLeft = auto;
                 //renderer.domElement.style.marginRight = auto;
                 document.body.appendChild(renderer.domElement);
 
                 const scene = new THREE.Scene();
+                scene.background = new THREE.Color(0x1E1E1E);
                 const camera = new THREE.PerspectiveCamera(75, (0.75*window.innerWidth)/(0.75*window.innerHeight), 0.1, 1000);
                 const orbit = new OrbitControls(camera, renderer.domElement);
                 camera.position.set(0, 2, 5);
                 orbit.update();
 
+                const planeGeometry = new THREE.PlaneGeometry(3, 3);
+                const planeMaterial = new THREE.MeshStandardMaterial({
+                    color: 0xffffff,
+                    side: THREE.DoubleSide
+                });
+                const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+                scene.add(plane);
+                plane.rotation.x= -0.5 * Math.PI;
+                plane.receiveShadow = true;
+
+                /*
                 const boxGeometry = new THREE.BoxGeometry();
                 const boxMaterial = new THREE.MeshBasicMaterial({
                     color: 0x00ff00
                 });
                 const box = new THREE.Mesh(boxGeometry, boxMaterial);
                 scene.add(box);
+                */
+
+                const loader = new GLTFLoader();
+                loader.castShadow = true;
+
+                loader.load('<?=ROOT?>img/characters/samplecharacter.glb', function (gltf) {
+                    scene.add(gltf.scene);
+                }, undefined, function (error) {
+                    console.error(error);
+                });
+
+                const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 2);
+                scene.add(directionalLight);
+                directionalLight.position.set(-30,20,10);
+                directionalLight.castShadow = true;
+                directionalLight.shadow.camera.bottom = -12;
+                const dLightHelper = new THREE.DirectionalLightHelper(directionalLight);
+                scene.add(dLightHelper);
+                const dLightShadowHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+                scene.add(dLightShadowHelper);
 
                 const axesHelper = new THREE.AxesHelper(5);
                 scene.add(axesHelper);
@@ -89,8 +123,8 @@
                 renderer.render(scene, camera);
 
                 function animate(time) {
-                    box.rotation.x = time / 1000;
-                    box.rotation.y = time / 1000;
+                    //box.rotation.x = time / 1000;
+                    //box.rotation.y = time / 1000;
 
                     renderer.render(scene, camera);
                 }
