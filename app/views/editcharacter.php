5b58f14cd79ca87f268f9a20fcbc346099e5d5e2
nrmalone <nicholasrmalone99@gmail.com>
2024-03-19 14:11:40 -0700

    character stat editing implemented

--- app/views/editcharacter.php
+++ app/views/editcharacter.php
@@ -9,13 +9,14 @@ DONE: name, race, class, alignment, level, STR, DEX, CON, INT, WIS, CHA, AC
 NEED: attribute modifiers, backstory, equipment, lifestyle, gold
 -->
     <div style="margin-top: 55px;">
-        <h1 align="center">Character Creation</h1>
+        <h1 align="center">Editing Character</h1>
         <div class="pcDiv" style="max-width: 100%; max-height: 50%; padding-top: 10px;">
-            <?php if (isset($_SESSION['userID'])): ?>
+            <?php if (isset($_SESSION['userID']) && is_int($data['pc'][0]->pcID)): ?>
+                <?php $character = $data['pc'][0] ?>
             <form method="POST">
+                <input type="hidden" name="pcID" value=<?=$character->pcID?>>
                 <!-- made a disabled & hidden submit button to prevent enter auto-submitting form -->
                 <button type="submit" disabled style="display: none;"></button>
-            <?php endif; ?>
                 <!-- left half character creation -->
                 <table style="display: inline; padding-left: 10px;">
                     <!-- Systems Reference Guide -->
@@ -30,14 +31,17 @@ NEED: attribute modifiers, backstory, equipment, lifestyle, gold
                     <!-- Name -->
                     <tr align="center">
                         <td class="pcCreationText">Name:&nbsp;</td>
-                        <td align="left"><input class="textField" name="name" type="text" maxlength="16" required></td>
+                        <td align="left"><input class="textField" name="name" type="text" maxlength="16" value="<?=$character->pcName?>" required></td>
                     </tr>
                     <!-- Race -->
                     <tr align="center">
                         <td class="pcCreationText">Race:&nbsp;</td>
                         <td align="left"><select class="textField" name="race">
+                            <?php echo ('<option value="' . $character->pcRace . '">' . $character->pcRace . ' (Current Race)</option>'); ?>
                             <?php foreach ($data['defaultRaces'] as $race) {
+                                if ($race !== $character->pcRace) {
                                 echo ('<option value="' . $race . '">' . $race . '</option>');
+                                }
                             }?>
                         </select></td>
                         <!--
@@ -74,8 +78,11 @@ NEED: attribute modifiers, backstory, equipment, lifestyle, gold
                             -->
                         <!-- Commenting out this section. Reading the classes and alignments from $data
                         was causing header conflicts when trying to redirect after submission -->
+                        <?php echo ('<option value="' . $character->pcClass . '">' . $character->pcClass . ' (Current Class)</option>'); ?>
                             <?php foreach($data['defaultClasses'] as $pcClass) {
+                                if ($pcClass !== $character->pcClass) {
                                 echo '<option value="' . $pcClass . '">' . $pcClass . '</option>';
+                                }
                             }?>
                         </select></td>
                     </tr>
@@ -84,8 +91,11 @@ NEED: attribute modifiers, backstory, equipment, lifestyle, gold
                     <tr align="center">
                         <td class="pcCreationText">Level:&nbsp;</td>
                         <td align="left"><select class="textField" name="level">
+                        <?php echo ('<option value="' . $character->pcLevel . '">' . $character->pcLevel . ' (Current Level)</option>'); ?>
                             <?php for ($i=1; $i<21; $i++) {
+                                if ($i !== intval($character->pcLevel)) {
                                 echo '<option value = ' . $i . '>' . $i . '</option>';
+                                }
                             } ?>
                         </select></td>
                         <td></td>
@@ -115,6 +125,9 @@ NEED: attribute modifiers, backstory, equipment, lifestyle, gold
                                 -->
                                 <!-- Commenting out this section. Reading the classes and alignments from $data
                                 was causing header conflicts when trying to redirect after submission -->
+                                <tr>
+                                    <td style="font-size: 10pt;">Currently <em><?=$character->pcAlignment?></em></td>
+                                </tr>
                                 <tr align="left">
                                 <?php for ($i=0; $i<count($data['alignments']); $i++) {
                                 if($i === 3) {
@@ -123,8 +136,8 @@ NEED: attribute modifiers, backstory, equipment, lifestyle, gold
                                 if ($i === 6) {
                                     echo '</tr><tr align="left">';
                                 }
-                                if ($data['alignments'][$i] === 'True Neutral') {
-                                    echo '<td class="pcAlignment"><input id="' . $data['alignments'][$i] . '" type="radio" name="alignment" value="' . $data['alignments'][$i] . '" checked><label for="' . $data['alignments'][$i] . '">' . $data['alignments'][$i] . '</label></td>';
+                                if ($data['alignments'][$i] == $character->pcAlignment) {
+                                    echo '<td class="pcAlignment"><input id="' . $data['alignments'][$i] . '" type="radio" name="alignment" value="' . $data['alignments'][$i] . '" checked><label for="' . $data['alignments'][$i] . '">' . $data['alignments'][$i]. '</label></td>';
                                 } else {
                                     echo '<td class="pcAlignment"><input id="' . $data['alignments'][$i] . '" type="radio" name="alignment" value="' . $data['alignments'][$i] . '""><label for="' . $data['alignments'][$i] . '">' . $data['alignments'][$i] . '</label></td>';
                                 }
@@ -151,57 +164,54 @@ NEED: attribute modifiers, backstory, equipment, lifestyle, gold
                     <tr align="center">
                         <td class="pcCreationText">Strength:&nbsp;</td>
                         <td>
-                            <p style="margin: 0;" id="strength">8</p><input type="hidden" id="strengthInput" name="strength">
+                            <p style="margin: 0; display: inline;" id="strength"><?=$character->pcSTR?></p><span style="font-size: 9pt;">&nbsp;(was <?=$character->pcSTR?>)</span><br><input type="hidden" id="strengthInput" name="strength" value="<?=$character->pcSTR?>">
                             <button class="attributeButton" type="button" onclick="decAttr('strength')">-</button>
                             <button class="attributeButton" type="button" onclick="incAttr('strength')">+</button>
                         </td>
-                        <td style="font-size: 10pt;"><strong>Basic Point Buy Rules:</strong></td>
+                        <td align="left" style="font-size: 10pt;">Min 8</strong></td>
                     </tr>
                     <!-- DEX -->
                     <tr align="center">
                         <td class="pcCreationText">Dexterity:&nbsp;</td>
                         <td>
-                            <p style="margin: 0;" id="dexterity">8</p><input type="hidden" id="dexterityInput" name="dexterity">
+                            <p style="margin: 0; display: inline;" id="dexterity"><?=$character->pcDEX?></p><span style="font-size: 9pt;">&nbsp;(was <?=$character->pcDEX?>)</span><br><input type="hidden" id="dexterityInput" name="dexterity" value="<?=$character->pcDEX?>">
                             <button class="attributeButton" type="button" onclick="decAttr('dexterity')">-</button>
                             <button class="attributeButton" type="button" onclick="incAttr('dexterity')">+</button>
                         </td>
-                        <td style="font-size: 10pt;">Min 8, Max 15 per attr.</td>
+                        <td align="left" style="font-size: 10pt;">Max 20</td>
                     </tr>
                     <!-- CON -->
                     <tr align="center">
                         <td class="pcCreationText">Constitution:&nbsp;</td>
                         <td>
-                            <p style="margin: 0;" id="constitution">8</p><input type="hidden" id="constitutionInput" name="constitution">
+                            <p style="margin: 0; display: inline;" id="constitution"><?=$character->pcCON?></p><span style="font-size: 9pt;">&nbsp;(was <?=$character->pcCON?>)</span><br><input type="hidden" id="constitutionInput" name="constitution" value="<?=$character->pcCON?>">
                             <button class="attributeButton" type="button" onclick="decAttr('constitution')">-</button>
                             <button class="attributeButton" type="button" onclick="incAttr('constitution')">+</button>
                         </td>
-                        <td style="font-size: 10pt;">Buying 14→15 costs 2pts</td>
                     </tr>
                     <!-- INT -->
                     <tr align="center">
                         <td class="pcCreationText">Intelligence:&nbsp;</td>
                         <td>
-                            <p style="margin: 0;" id="intelligence">8</p><input type="hidden" id="intelligenceInput" name="intelligence">
+                            <p style="margin: 0; display: inline;" id="intelligence"><?=$character->pcINT?></p><span style="font-size: 9pt;">&nbsp;(was <?=$character->pcINT?>)</span><br><input type="hidden" id="intelligenceInput" name="intelligence" value="<?=$character->pcINT?>">
                             <button class="attributeButton" type="button" onclick="decAttr('intelligence')">-</button>
                             <button class="attributeButton" type="button" onclick="incAttr('intelligence')">+</button>
                         </td>
-                        <td style="font-size: 10pt;"><p style="margin: 0;" id="remainingPts">Remaining Pts: 27</p></td>
                     </tr>
                     <!-- WIS -->
                     <tr align="center">
                         <td class="pcCreationText">Wisdom:&nbsp;</td>
                         <td>
-                            <p style="margin: 0;" id="wisdom">8</p><input type="hidden" id="wisdomInput" name="wisdom">
+                            <p style="margin: 0; display: inline;" id="wisdom"><?=$character->pcWIS?></p><span style="font-size: 9pt;">&nbsp;(was <?=$character->pcWIS?>)</span><br><input type="hidden" id="wisdomInput" name="wisdom" value="<?=$character->pcWIS?>">
                             <button class="attributeButton" type="button" onclick="decAttr('wisdom')">-</button>
                             <button class="attributeButton" type="button" onclick="incAttr('wisdom')">+</button>
                         </td>
-                        <td style="font-size: 10pt;">We'll auto-calculate modifiers later</td>
                     </tr>
                     <!-- CHA -->
                     <tr align="center">
                         <td class="pcCreationText">Charisma:&nbsp;</td>
                         <td>
-                            <p style="margin: 0;" id="charisma">8</p><input type="hidden" id="charismaInput" name="charisma">
+                            <p style="margin: 0; display: inline;" id="charisma"><?=$character->pcCHA?></p><span style="font-size: 9pt;">&nbsp;(was <?=$character->pcCHA?>)</span><br><input type="hidden" id="charismaInput" name="charisma" value=<?=$character->pcCHA?>>
                             <button class="attributeButton" type="button" onclick="decAttr('charisma')">-</button>
                             <button class="attributeButton" type="button" onclick="incAttr('charisma')">+</button>
                         </td>
@@ -233,22 +243,49 @@ NEED: attribute modifiers, backstory, equipment, lifestyle, gold
                             </optgroup>
                         </select></td>
                         <td align="left"><input type="checkbox" class="textField" id="shield" name="shield" value="2"><label for="shield" style="font-size: 10pt;" >Shield (+2)</label></td>
+                        <td style="    background-color: #6A0F0F; border-radius: 0 8px 8px 0; padding: 0 4px 4px 4px;">Current AC: <?=$character->pcAC?></td>
                     </tr>
                     <tr>
                         <td></td>
                         <td align="center">
-                            <?php if (isset($_SESSION['userID'])) {
-                                echo '<button class="accountButton" style="margin-top: 15px;">Submit</button>'; }
-                                else { echo '<h4 style="background-color: #1E1E1E; border: 1px white solid; border-radius: 10px 0 10px 0; padding: 5px 5px 5px 5px;">You must <a href="' . ROOT . 'account/signin" style="color: white; text-decoration: none; text-decoration: underline;">sign in</a><br> to save your <br>character sheet.</h4>'; } ?>
+                            <button class="accountButton" style="margin-top: 15px;">Submit</button>
                         </td>
                     </tr>
                 </table>
             </form>
+            <?php elseif (!isset($_SESSION['userID'])): ?>
+                <h3 align="center">Whoops. You can't edit characters if you're not <a href="<?=ROOT?>account/signin" style="color: white; text-decoration: none; text-decoration: underline;">signed in</a>.</h3>
+            <?php endif; ?>
         </div>
     </div>
 </div>
 
-<script src="<?=ROOT?>js/charattribs.js"></script>
+<script type="text/javascript">
+    function decAttr(attr) {
+        attrP = document.getElementById(attr);
+        currentAttr = parseInt(attrP.textContent);
+        if (currentAttr > 8) {
+        newAttr = currentAttr - 1;
+        } else {
+            newAttr = currentAttr;
+        }
+        attrInput = document.getElementById(attr + 'Input');
+        attrInput.value = newAttr;
+        attrP.textContent = newAttr;
+    }
+    function incAttr(attr) {
+        attrP = document.getElementById(attr);
+        currentAttr = parseInt(attrP.textContent);
+        if (currentAttr < 20) {
+        newAttr = currentAttr + 1;
+        } else {
+            newAttr = currentAttr;
+        }
+        attrInput = document.getElementById(attr + 'Input');
+        attrInput.value = newAttr;
+        attrP.textContent = newAttr;
+    }
+</script>
 
 <?php require_once '../app/components/pagefooter.php';
 //ob_start();?>
