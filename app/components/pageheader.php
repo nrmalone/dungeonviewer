1802274114dde3b155e41e23d96bcb382e760c57
nrmalone <nicholasrmalone99@gmail.com>
2024-06-07 19:14:55 -0700

    setting expiry for cache to avoid image overwrites not showing

--- app/components/pageheader.php
+++ app/components/pageheader.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Cache-Control" content="max-age=900 , must-revalidate">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $data['title_page']?></title>
 
